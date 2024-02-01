79515c9f1dad9a9f37de2e4a6c88687bbeeaf0e6
大塚　竜太郎 <otsukaryutaro@team-lab.com>
2024-02-01 16:12:05 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import styles from './page.module.css';
 
 export default function Home() {
+  // コメント
   return (
     <main className={styles['main']}>
       <div className={clsx(styles['wrapper'],styles['wrapper-freeza'])}>
